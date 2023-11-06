85034e6c3daf3607834f6ca960c35e31fa2d7d9e
Joseph Abreau <joseph.abreau@gmail.com>
2023-11-06 11:39:45 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const num1 = 2000;
+const num2 = 100;
+const num3 = 6;
+const num4 = 10;
+const num5 = 90;
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+const square = num3 => num3 * num3
+
+const add = (num4, num5) => num4 + num5
